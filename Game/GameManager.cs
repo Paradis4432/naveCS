29e243b58015e8a8df925d1fd74054c1a204f26e
alu704 <juanba1753@gmail.com>
2023-06-14 20:27:02 -0300

    cambio de nombre

---
+++ Game/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    
+    class GameManager {
+        private GameManager instance; 
+
+        public GameManager getInstance()
+        {
+            return this.instance;
+        }
+
+        public GameManager()
+        {
+            this.instance = this;
+        }
+
+
+
+
+
+    }
+}
