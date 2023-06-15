744d20c2cac9851cef4b6a017bed620401d27f9c
santi <santischneider1@gmail.com>
2023-06-14 21:38:13 -0300

    name change

--- Game/GameManager.cs
+++ Game/GameManager.cs
@@ -8,7 +8,22 @@ namespace Game
 {
     
     class GameManager {
+
+        public Ship ship;
+        Cuerpo shipCuerpo; // cuerpo de ship
+        private GameStage stage;
+        private int secsCounter = 0;
+        private List<Meteors> meteors = new List<Meteors>();
+        private List<Bullet> bulletsShoot = new List<Bullet>();
+        private int points = 0;
+        public Boolean gameover  = false;
+
+        static private System.Threading.Timer timer;
+        static private System.Threading.Timer checkColls;
+
         private GameManager instance; 
+
+        public Ship getShip() { return ship; }
 
         public GameManager getInstance()
         {
@@ -18,11 +33,133 @@ namespace Game
         public GameManager()
         {
             this.instance = this;
+
+            this.ship = Ship.GetInstance();
+            timer = new System.Threading.Timer(OnTimedEvent, null, 0, 1000);
+            checkColls = new System.Threading.Timer(CheckColls, null, 0, 100);
+
         }
 
+        public void updateMenu()
+        {
+            Engine.Clear();
+            Engine.Draw(Engine.GetTexture("menu.png"), 0, 0, 1, 1, 0, 0, 0);
+            Engine.Show();
+        }
+
+        public void updateWin()
+        {
+            Engine.Clear();
+
+            Engine.Debug(points);
+            Engine.Draw(Engine.GetTexture("win.png"), 0, 0);
+
+            Engine.Show();
+
+            if (Engine.GetKey(Keys.R)) stage = GameStage.Menu;
+        }
+        public void resetValues()
+        {
+            stage = GameStage.Gameplay;
+            ship.Reset();
+            meteors = new List<Meteors>();
+            secsCounter = 0;
+        }
+
+        public void updateGameplay()
+        {
+            if (secsCounter >= 30 && !ship.exploded)
+            {
+                stage = GameStage.Win;
+                this.gameover = true;
+            }
+            //Engine.Debug(mets.Count);
+
+            shipCuerpo = ship.Nave;
+            Update();
+            Draw();
+
+        }
+
+        public void setStage(GameStage newStage) {
+            this.stage = newStage;
+        }
+
+        public GameStage getStage()
+        {
+            return this.stage;
+        }
+            
+
+        private void OnTimedEvent(Object stateInfo)
+        {
+            if (stage != GameStage.Gameplay) return;
+
+            meteors.Add(new Meteors(shipCuerpo.pos, new Vector2(50, 1)));
+            secsCounter++;
+        }
+
+        private void CheckColls(Object stateInfo)
+        {
+            // update every 100 mils
+            foreach (var met in meteors.ToList())
+            {
+                // if any met is colliding with ship.pos
+                //Vector2 shipRealPos = new Vector2(0,0);
+                //shipRealPos.x = CS.pos.x + (ship.imgW / 2);
+                //shipRealPos.y = CS.pos.y + (ship.imgH / 2);
+
+                if (!met.met.alive) meteors.Remove(met);
+
+                foreach (var bull in bulletsShoot.ToList())
+                {
+
+                    if (!bull.Bala.alive) bulletsShoot.Remove(bull);
 
 
+                    if (Vector2.Colliding(met.met.pos, bull.Bala.pos, met.met.rad - 4, bull.Bala.rad - 3))
+                    {
+                        bulletsShoot.Remove(bull);
+                        meteors.Remove(met);
+                        points++;
+                    }
 
+
+                }
+
+                if (Vector2.Colliding(met.met.pos, shipCuerpo.pos, met.met.rad - 4, shipCuerpo.rad - 6))
+                {
+                    //stage = GameStage.Lost;
+                    ship.exploded = true;
+                }
+            }
+        }
+
+        void Update()
+        {
+            ship.Update();
+
+            foreach (var met in meteors.ToList()) met.Update();
+            foreach (var bull in bulletsShoot.ToList()) bull.Update();
+
+            //Engine.Debug("---");
+        }
+
+        void Draw()
+        {
+            Engine.Clear();
+
+            Engine.Draw(Engine.GetTexture("background.png"), 0, 0);
+            if (ship.exploded) Engine.Draw(Engine.GetTexture("lost.png"), 300, 100);
+
+            ship.Draw();
+            foreach (var met in meteors.ToList())
+            {
+                met.Draw();
+            }
+
+            Engine.Show();
+        }
 
     }
 }
--- Game/Program.cs
+++ Game/Program.cs
@@ -17,73 +17,45 @@ namespace Game
      */
     public class Program
     {
-        static Ship ship;
-        static Cuerpo CS; // cuerpo de ship
-        static private GameStage stage;
-        static private int secsCounter = 0;
-        static List<Meteors> mets = new List<Meteors>();
-        static public List<Bullet> bulletsShoot = new List<Bullet>();
-        static private int points = 0;
 
-        static private System.Threading.Timer timer;
-        static private System.Threading.Timer checkColls;
-
+        private static GameManager gameManager = null;
         static void Main(string[] args)
         {
             Engine.Initialize();
-            stage = GameStage.Menu;
+            //stage = GameStage.Menu;
+            gameManager = new GameManager();
+
 
             //ship = new Ship(new Vector2(375, 275));
-            ship = Ship.GetInstance();
+            //ship = Ship.GetInstance();
 
-            timer = new System.Threading.Timer(OnTimedEvent, null, 0, 1000);
-            checkColls = new System.Threading.Timer(CheckColls, null, 0, 100);
+            //timer = new System.Threading.Timer(OnTimedEvent, null, 0, 1000);
+            //checkColls = new System.Threading.Timer(CheckColls, null, 0, 100);
 
            
 
-            while (true)
+            while (gameManager.gameover)
             {
-                switch (stage) {
+                switch (gameManager.getStage()) {
                     case GameStage.Menu:
-                        Engine.Clear();
-                        Engine.Draw(Engine.GetTexture("menu.png"), 0, 0, 1, 1, 0, 0, 0);
-                        Engine.Show();
-    
+                        gameManager.updateMenu();
                         if (!(Engine.GetKey(Keys.E))) continue;
-                        stage = GameStage.Gameplay;
-                        ship.Reset();
-                        mets = new List<Meteors>();
-                        secsCounter = 0;
+                        gameManager.resetValues();
                         break;
+                        
                     case GameStage.Gameplay:
-
-                        if (Engine.GetKeyDown(Keys.R) && ship.exploded)
+                        if (Engine.GetKeyDown(Keys.R) && gameManager.getShip().exploded)
                         {
-                            stage = GameStage.Menu;
+                            //stage = GameStage.Menu;
+                            gameManager.setStage(GameStage.Menu);
 
                             continue;
                         }
-
-                        if (secsCounter >= 30 && !ship.exploded) {
-                            stage = GameStage.Win;
-                            break;
-                        }
-                        Engine.Debug(mets.Count);
-
-                        CS = ship.Nave;
-                        Update();
-                        Draw();
+                        gameManager.updateGameplay();
                         
                         break;
                     case GameStage.Win:
-                        Engine.Clear();
-
-                        Engine.Debug(points);
-                        Engine.Draw(Engine.GetTexture("win.png"), 0, 0);
-
-                        Engine.Show();
-
-                        if (Engine.GetKey(Keys.R)) stage = GameStage.Menu;
+                        gameManager.updateWin();
 
 
                         break;
@@ -91,73 +63,6 @@ namespace Game
                 }
 
             }
-        }
-
-        private static void OnTimedEvent(Object stateInfo) {
-            if (stage != GameStage.Gameplay) return;
-
-            mets.Add(new Meteors(CS.pos, new Vector2(50, 1)));
-            secsCounter++;
-        }
-
-        private static void CheckColls(Object stateInfo) {
-            // update every 100 mils
-            foreach (var met in mets.ToList()) {
-                // if any met is colliding with ship.pos
-                //Vector2 shipRealPos = new Vector2(0,0);
-                //shipRealPos.x = CS.pos.x + (ship.imgW / 2);
-                //shipRealPos.y = CS.pos.y + (ship.imgH / 2);
-
-                if (!met.met.alive) mets.Remove(met);
-
-                foreach (var bull in bulletsShoot.ToList()) {
-
-                    if (!bull.Bala.alive) bulletsShoot.Remove(bull);
-
-
-                    if (Vector2.Colliding(met.met.pos, bull.Bala.pos, met.met.rad - 4, bull.Bala.rad - 3)) {
-                        bulletsShoot.Remove(bull);
-                        mets.Remove(met);
-                        points++;
-                    }
-
-
-                }
-
-                if (Vector2.Colliding(met.met.pos, CS.pos, met.met.rad - 4, CS.rad - 6)) {
-                    //stage = GameStage.Lost;
-                    ship.exploded = true;
-                }
-            }
-        }
-
-        //static Vector2 tempMetLostPos;
-
-        static void Update() {
-            ship.Update();
-
-            foreach (var met in mets.ToList()) met.Update();
-            foreach (var bull in bulletsShoot.ToList()) bull.Update();
-
-            //Engine.Debug("---");
-        }
-
-        static void Draw() {
-            Engine.Clear();
-
-            Engine.Draw(Engine.GetTexture("background.png"), 0, 0);
-            if(ship.exploded) Engine.Draw(Engine.GetTexture("lost.png"), 300, 100);
-
-            ship.Draw();
-            foreach (var met in mets.ToList()) {
-                met.Draw();
-            }
-
-            Engine.Show();
-        }
-
-        public static void SetStage(GameStage s) {
-            stage = s;
         }
     }
 }
